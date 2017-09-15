22ad55b4fea72ec018de65022eb9ed3cfc91fedc
pravkhande <preetika.ravkhande@reancloud.com>
2017-09-15 19:11:12 +0530

    changes in run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@ sudo bundle install
 
   
 inspec exec inspec-code -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format html >> report/results.html
-inspec exec inspec-code -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json-rspec >> report/results.json
+#inspec exec inspec-code -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json-rspec >> report/results.json
+
+inspec exec inspec-code -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json >> report/results.json
+
