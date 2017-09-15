3cb94967eae5f384f32d40166b145a4c1a7c29cd
pravkhande <preetika.ravkhande@reancloud.com>
2017-09-15 13:25:01 +0530

    code change

--- inspec-code/controls/example.rb
+++ inspec-code/controls/example.rb
@@ -8,16 +8,6 @@ describe file('/tmp') do
   it { should be_directory }
 end
 
-# you add controls here
-control 'inspec-example' do                        # A unique ID for this control
-  impact 0.1                                # The criticality, if this control fails.
-  title 'inspec-example'             # A human-readable title
-  desc 'An optional description...'
-  describe file('/tmp') do                  # The actual test
-    it { should be_directory }
-  end
-end
-
 describe windows_feature('DHCP Server') do
   it { should be_installed }
 end
@@ -28,15 +18,6 @@ end
 
 describe user('Administrator') do
   it { should exist }
-end
-
-describe package('chefdk') do
-  it { should be_installed }
-  its('version') { should eq '1.19.2.1' }
-end
-
-describe port(80) do
-  it { should be_listening }
 end
 
 describe file('C:\Program Files\Java\jdk1.8.0_144\bin\java.exe') do
