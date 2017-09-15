b9d0a3c4c6a7b6d98cf196e6bd6c85510a9fafc4
pravkhande <preetika.ravkhande@reancloud.com>
2017-09-15 17:46:22 +0530

    remove inspec command from run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sudo gem install bundler
 sudo bundle install
-inspec
+
   
 inspec exec inspec-code -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format html >> report/results.html
 inspec exec inspec-code -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json-rspec >> report/results.json
