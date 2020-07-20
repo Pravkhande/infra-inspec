4a06014c0820101a322e7088f57548359436db3e
pravkhande <pravkhande@gmail.com>
2020-07-21 00:28:29 +0530

    run command

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 sudo gem install bundler
 sudo bundle install
 
-  
-inspec exec inspec-code -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format html >> report/results.html
-inspec exec inspec-code -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json-rspec >> report/results.json
+inspec exec inspec-code -t winrm://Administrator@54.81.107.52 --password "SB=SRhZ*;ZDZKZ2" --format html >> report/results.html  
+inspec exec inspec-code -t winrm://Administrator@54.81.107.52 --password "SB=SRhZ*;ZDZKZ2" --format html >> report/results.json
 
+#inspec exec inspec-code -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format html >> report/results.html
+#inspec exec inspec-code -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json-rspec >> report/results.json
 #inspec exec inspec-code -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json >> report/results.json
 
